4deeb93b471e2447fbe228417fa3fbaaacf7efc9
CMBill <callmebill@billw.cn>
2024-07-11 16:19:34 +0800

    clean file

--- demos/demo.cpp
+++ demos/demo.cpp
@@ -46,15 +46,6 @@ std::vector<std::vector<float>> readCSV(const std::filesystem::path& filepath) {
     return data;
 }
 
-void printData(const std::vector<std::vector<float>>& data) {
-    for (const auto& row : data) {
-        for (const auto& val : row) {
-            std::cout << val << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
 float sendCmd(const std::vector<float>& cmds, const std::string& serial_port) {
     SerialPort  serial(serial_port);
     MotorCmd    cmd;
